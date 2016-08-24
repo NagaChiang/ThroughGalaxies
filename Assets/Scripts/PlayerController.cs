87baf0181a22d5f34a1e3bb0e77d43af2af2a096
Naga Chiang <chchiang.cs03g@g2.nctu.edu.tw>
2016-08-24 21:01:45 +0800

    Add background and player control

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public struct Boundary
+{
+    public float xMin, xMax;
+    public float zMin, zMax;
+}
+
+public class PlayerController : MonoBehaviour {
+
+    public float moveSpeed;
+    public float tiltLimit;
+    public Boundary boundary;
+
+	void FixedUpdate()
+    {
+        // handle the movements
+        move();
+    }
+
+    // handle the movement of the plane
+    private void move()
+    {
+        Rigidbody rigidbody = GetComponent<Rigidbody>();
+
+        float axisHorizontal = Input.GetAxis("Horizontal");
+        float axisVertical = Input.GetAxis("Vertical");
+
+        // update velocity
+        Vector3 movement = new Vector3(axisHorizontal, 0.0f, axisVertical);
+        rigidbody.velocity = moveSpeed * movement;
+
+        // update position
+        rigidbody.position = new Vector3
+        (
+            Mathf.Clamp(rigidbody.position.x, boundary.xMin, boundary.xMax),
+            rigidbody.position.y,
+            Mathf.Clamp(rigidbody.position.z, boundary.zMin, boundary.zMax)
+        );
+
+        // update rotation
+        rigidbody.rotation = Quaternion.Euler(0.0f, 0.0f, rigidbody.velocity.x * -tiltLimit);
+    }
+}
