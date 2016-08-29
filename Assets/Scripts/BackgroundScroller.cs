8efeeb994154d43723da504a86882c2f01c4b3d6
Naga Chiang <chchiang.cs03g@g2.nctu.edu.tw>
2016-08-29 16:55:25 +0800

    Add starfield and scrolling background

---
+++ Assets/Scripts/BackgroundScroller.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundScroller : MonoBehaviour {
+
+    public float scrollSpeed;
+    public float backgroundHeight;
+
+    private Vector3 _positionStart;
+
+	void Start ()
+    {
+        _positionStart = transform.position;
+    }
+	
+	void Update ()
+    {
+        float newZ = Mathf.Repeat(Time.time * scrollSpeed, backgroundHeight);
+        transform.position = _positionStart + Vector3.back * newZ;
+    }
+}
