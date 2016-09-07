befdca34620da261a71c4bd92672068b91a86d7c
Naga Chiang <chchiang.cs03g@g2.nctu.edu.tw>
2016-09-07 20:38:52 +0800

    Add forces to powerups when player gets closer

--- Assets/Scripts/Powerups/Powerup.cs
+++ Assets/Scripts/Powerups/Powerup.cs
@@ -6,6 +6,12 @@ public abstract class Powerup : MonoBehaviour {
     public float verticalSpeed;
     public float rotateFactor;
 
+    // player collecting prefernces
+    private const float COLLECT_FORCE = 15.0f;
+    private const float COLLECT_RANGE = 5.0f;
+
+    private bool isLooted;
+
     void Start()
     {
         // random speed
@@ -14,18 +20,61 @@ public abstract class Powerup : MonoBehaviour {
 
         // random rotation
         rigidbody.angularVelocity = Random.insideUnitSphere * rotateFactor;
+
+        // looted or not
+        isLooted = false;
+    }
+
+    void FixedUpdate()
+    {
+        if (!isLooted)
+        {
+            // move towards player within a certain range
+            collected(COLLECT_FORCE, COLLECT_RANGE);
+        }
     }
 
     void OnTriggerEnter(Collider other)
     {
         // only for player
-        if(other.tag == "Player")
+        if(other.tag == "Player" && !isLooted)
         {
+            // mark as looted
+            isLooted = true;
+
             // do power up effects
             doPowerup(other.gameObject.GetComponent<PlayerController>());
 
-            // destroy this
+            // destroy
             Destroy(gameObject);
+        }
+    }
+
+    private void collected(float force, float range)
+    {
+        // get player
+        GameObject objPlayer = GameObject.FindWithTag("Player");
+        if (objPlayer == null)
+            return;
+        Vector3 posPlayer = objPlayer.transform.position;
+
+        // check distance
+        float distance = Vector3.Distance(transform.position, posPlayer);
+        if (distance <= range)
+        {
+            // calculate the force applied to the powerup
+            Vector3 forceToPlayer = (posPlayer - transform.position).normalized; // direction
+            forceToPlayer *= force;
+
+            // apply force to this powerup
+            GetComponent<Rigidbody>().AddForce(forceToPlayer);
+        }
+
+        // out of range
+        else
+        {
+            // zero out the velocity
+            GetComponent<Rigidbody>().velocity = Vector3.zero;
         }
     }
 
