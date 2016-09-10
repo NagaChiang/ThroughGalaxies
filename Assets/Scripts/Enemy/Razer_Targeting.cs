e15aa4813f4dff70aa0a7f359752c8044c63b51b
Naga Chiang <chchiang.cs03g@g2.nctu.edu.tw>
2016-09-10 09:49:59 +0800

    Add targeting enemy

---
+++ Assets/Scripts/Enemy/Razer_Targeting.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Razer_Targeting : Enemy {
+
+    public float initialSpeed;
+    public float verticalSpeed;
+    public SimpleWeapon weapon;
+    public float firingDelay;
+    public Limit durationStart;
+
+    private Rigidbody _rigidbody;
+
+    new void Start()
+    {
+        // from Enemy (also Damageable)
+        base.Start();
+
+        // move down (-z)
+        _rigidbody = GetComponent<Rigidbody>();
+        _rigidbody.velocity = Vector3.back * initialSpeed;
+
+        // keep looking at the player and firing
+        StartCoroutine(targeting());
+    }
+
+    private IEnumerator targeting()
+    {
+        // start wait
+        yield return new WaitForSeconds(Random.Range(durationStart.min, durationStart.max));
+
+        // change the velocity
+        _rigidbody.velocity = Vector3.back * verticalSpeed;
+
+        // keep firing
+        StartCoroutine(keepFiring());
+        
+        // keep targeting player
+        GameObject objPlayer = GameObject.FindWithTag("Player");
+        if (objPlayer == null)
+            Debug.LogError("Can't find the object of player.");
+        else
+        {
+            while (true)
+            {
+                transform.LookAt(objPlayer.transform.position);
+                yield return null;
+            }
+        }
+    }
+
+    private IEnumerator keepFiring()
+    {
+        // little delay for look at
+        yield return new WaitForSeconds(firingDelay);
+
+        while (true)
+        {
+            weapon.fire();
+            yield return new WaitForSeconds(weapon.fireCooldown);
+        }
+    }
+}
